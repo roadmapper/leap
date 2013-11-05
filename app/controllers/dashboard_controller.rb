7795c88c6b2172ffb5f4ab26ddd3657be3714b49
Vinay Dandekar <vindansam@hotmail.com>
2013-11-04 23:46:55 -0500

    Finished adding upload functionality, UTF-8 encoding may not work

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,11 @@ class DashboardController < ApplicationController
   def index
     @property = Property.where(:owner_name => "Vinay")
   end
-
+  def upload
+    uploaded_io = params[:picture]
+    File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'w') do |file|
+      file.write(uploaded_io.read)
+    end
+    render :text => "File has been uploaded successfully"    
+  end
 end
