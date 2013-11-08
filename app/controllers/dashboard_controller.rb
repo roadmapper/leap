0c335b328fe4b19e8c298de2c71ba489225fa221
Vinay Dandekar <vvd5gc@pegasus.cs.virginia.edu>
2013-11-08 10:14:29 -0500

    Added jQuery gem and updated a file name attribute in the dashboard upload.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
     @property = Property.where(:owner_name => "Vinay")
   end
   def upload
-    uploaded_io = params[:picture]
+    uploaded_io = params[:file]
     File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'w') do |file|
       file.write(uploaded_io.read)
     end
