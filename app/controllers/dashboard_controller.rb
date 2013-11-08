531ec714372d0b943c7bc435719d367ca2253d73
Vinay Dandekar <vindansam@hotmail.com>
2013-11-08 10:36:12 -0500

    Added redirect after upload in dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,8 +5,12 @@ class DashboardController < ApplicationController
   def upload
     uploaded_io = params[:file]
     File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'w') do |file|
+      file.force_encoding('UTF-8')
       file.write(uploaded_io.read)
     end
     render :text => "File has been uploaded successfully"    
+    update_page do |page|
+      page.redirect_to(:action => 'index')
+    end
   end
 end
