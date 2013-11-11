ff541d18d95a170bee6743a6ab923471774443e0
Eric Tsai <ekt7be@virginia.edu>
2013-11-11 12:26:16 -0500

    Revert "test revision2"
    This reverts commit 24dd1034f9c3dbb00e84400d66b7f970420f2b41.

--- app/models/record_lookup.rb
+++
@@ -1,4 +0,0 @@
-class RecordLookup < ActiveRecord::Base
-  belongs_to :property
-  belongs_to :utility_type
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Public::Application.config.secret_token = '076415bfe549203bd85ccc974afe3cd35dfe609c51519226dd1a26765aea25ef18fa374c4002ce4fd1dde51977c4ad317cc321394f2fc35a19fab4fbd95ac4fa'
