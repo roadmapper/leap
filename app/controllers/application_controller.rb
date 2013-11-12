6612bd20a78ed9122511c010618f4c5e8bcdce0c
Renee Seaman <res6tq@cassiopeia.(none)>
2013-11-11 19:22:23 -0500

    user login updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  USER, PASSWORD = 'dhh', 'secret'
+  before_filter :authentication_check   #, :except => :index
+
   protect_from_forgery
+
+
+  private
+  def authentication_check
+	authenticate_or_request_with_http_basic do |user, password|
+	user == USER && password == PASSWORD
+	end
+  end
 end
