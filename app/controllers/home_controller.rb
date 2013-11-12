e9fad97a6de0f065e55a59032b39e31bfa5278e7
Renee Seaman <res6tq@virginia.edu>
2013-11-11 19:23:43 -0500

    user login updated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
+  USER, PASSWORD = 'dhh', 'secret'
+  before_filter :authentication_check   #, :except => :index
+
   def index
+  end
+
+  private
+  def authentication_check
+	authenticate_or_request_with_http_basic do |user, password|
+	user == USER && password == PASSWORD
+	end
   end
 
 end
