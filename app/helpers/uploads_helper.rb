ac094d3495ad0b0af2a559dd2e8e77029dbb771c
cpn2pf <cpn2pf@cassiopeia.(none)>
2013-12-01 22:49:12 -0500

    Upload with xls to csv

--- app/helpers/uploads_helper.rb
+++ app/helpers/uploads_helper.rb
@@ -4,11 +4,11 @@ require 'roo'
 
 	# change excel files to csv
 	def xls2csv
-	    Dir["/tmp/*.xls"].each do |file|  
+	    Dir["/leap/uploads/*.xls"].each do |file|  
 	      file_path = "#{file}"
 	      file_basename = File.basename(file, ".xls")
 	      xls = Excel.new(file_path)
-	      xls.to_csv = ("/tmp/#{file_basename}.csv")
+	      xls.to_csv = ("/leap/uploads/#{file_basename}.csv")
 	    end
 	end
 end
