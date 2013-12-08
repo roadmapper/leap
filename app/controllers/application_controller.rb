d6389980a7cd31fb8002432fafeecddbeb55dcaa
Renee Seaman <res6tq@cassiopeia.(none)>
2013-12-08 16:05:26 -0500

    redirection after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(user)
 	#redirect to home
-	#"/leap/"
-	""
+	"/leap/"
+	#""
 	#has to be "" on local machine
   
   end
