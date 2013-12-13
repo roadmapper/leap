cf53b58affd5968f7828bbe3e75323e589eedc32
Renee Seaman <res6tq@cassiopeia.(none)>
2013-12-13 12:13:31 -0500

    bootstrap and web page styling

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,8 @@ module Public
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
+    config.assets.enabled = true
+
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
   end
