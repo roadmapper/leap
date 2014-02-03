6999c5d12fd6b09aa6e2b00663207665a9d56f00
Vinay Dandekar <leap@cassiopeia.(none)>
2014-02-03 15:31:42 -0500

    Reverting to the state of the project at 96c2a7d

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,5 @@ module Public
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-    config.assets.enabled = true
   end
 end
