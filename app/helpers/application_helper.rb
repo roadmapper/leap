2d1a558b8c05e79fc3e34883f73897b93da3c262
James Ashe <leap@slp.(none)>
2014-02-02 22:40:45 -0500

    column sorting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@ module ApplicationHelper
 	def title(page_title)
 		content_for(:title) { page_title }
 	end
+
+	def sortable(column, title = nil)
+        title ||= column.titleize
+        css_class = column == sort_column ? "current #{sort_direction}" : nil
+        direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+        link_to "#{title} <i class='#{direction == "desc" ? "icon-chevron-down" : "icon-chevron-up"}'></i>".html_safe, {:sort => column, :direction => direction}, {:class => css_class}
+	end
 end
