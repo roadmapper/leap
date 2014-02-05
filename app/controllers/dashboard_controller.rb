cec0407258bd27b1cfb5904ac9ae8ae9eb17b2d1
Cameron Nye <cpn2pf@virginia.edu>
2014-02-04 22:16:59 -0500

    Fixed some bugs, upload should add to records. Flaws: Only does this for Dominion records right now( If they're formatted right), but can expand to gas easily. Only does the first sheet (can expand that to do all sheets in a bit, but was easier to do the first sheet only). Hard to test on pegasus server (not sure how to do this without messing up recordingswith fake test information), but works on local machine.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -42,8 +42,8 @@ class DashboardController < ApplicationController
                     $i = xlsx.sheets.length - 1
                     while $i >= 0 do
                         xlsx.default_sheet = xlsx.sheets[$i]
-			csvName = "/#{file_basename}#{$i}.csv"
-			csvPath = path + name
+			csvName = "#{file_basename}#{$i}.csv"
+			csvPath = path + "/" + csvName
                         xlsx.to_csv(csvPath)
                         Upload.where(:file_name => "#{file_basename}#{$i}.csv", :status => 'Not Processed', :upload_date => Time.now).first_or_create(:locked => false)
                         $i -=1
@@ -53,28 +53,30 @@ class DashboardController < ApplicationController
                     end
                     #turn on if want to remove xlsx file after conversion (probably want to keep off so can check if xlsx file is already uploaded)
                     #FileUtils.remove(file)
+
+############temp location############################
+		Thread.new do
+			fields_to_insert = %w{ ID_BA DT_READ AMT_KWH DAYS_USED ContractAcct. Consumption }
+			rows_to_insert = []
+		
+			CSV.foreach(csvPath, headers: true) do |row|
+			  row_to_insert = row.to_hash.select { |k, v| fields_to_insert.include?(k) }
+			 #row.to_hash.values_at(*fields_to_insert)
+			  #rows_to_insert << row_to_insert
+
+			  stringdate = row_to_insert["DT_READ"]
+		          date = DateTime.new(1899,12,30) + Integer(stringdate).days 
+			  
+	   		  #formatted_date = date.strftime('%a %b %d %Y')
+			  
+			Recording.where({"acctnum"=>row_to_insert["ID_BA"], "consumption"=>row_to_insert["AMT_KWH"], "days_in_month"=>row_to_insert["DAYS_USED"], "read_date"=>date}).first_or_create(:locked => false)
+			end
+			Upload.update_all( {:status => 'Processed'}, {:file_name => csvName})
+		end
+############temp location############################
                 end
 	        
-############temp location############################
-	Thread.new.do
-		fields_to_insert = %w{ ID_BA DT_READ AMT_KWH DAYS_USED ContractAcct. Consumption }
-		rows_to_insert = []
-		
-		CSV.foreach(csvPath, headers: true) do |row|
-		  row_to_insert = row.to_hash.select { |k, v| fields_to_insert.include?(k) }
-		 #row.to_hash.values_at(*fields_to_insert)
-		  #rows_to_insert << row_to_insert
 
-		  stringdate = row_to_insert["DT_READ"]
-                  date = DateTime.new(1899,12,30) + Integer(stringdate).days 
-		  
-   		  #formatted_date = date.strftime('%a %b %d %Y')
-		  
-		Recording.where({"acctnum"=>row_to_insert["ID_BA"], "consumption"=>row_to_insert["AMT_KWH"], "days_in_month"=>row_to_insert["DAYS_USED"], "read_date"=>date}).first_or_create(:locked => false)
-		end
-		Upload.update_all( {:status => 'Processed'}, {:file_name => csvName})
-	end
-############temp location############################
                 elsif File.exists?(path + "//" + filename)
                 flash[:notice] = "Duplicate file exists. File was not uploaded successfully to: " + path
                 else
