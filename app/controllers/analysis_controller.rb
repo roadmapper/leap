013dae08eca18bbd92d20a615f2369bce6455d25
Vinay Dandekar <vindansam@hotmail.com>
2014-03-03 02:25:07 -0500

    Adding null accounts

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -3,4 +3,12 @@ class AnalysisController < ApplicationController
   def index
   end
 
+  def null_accounts
+    @accounts = Report::NullAccount.paginate(:page => params[:page])
+    respond_to do |format|
+      format.js
+      ajax_respond format, :section_id => "null_accounts"
+    end
+  end
+
 end
