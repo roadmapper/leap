131a16e8125a4bb0d25f45618988e594b830f7a7
cpn2pf <cpn2pf@cassiopeia.(none)>
2014-03-17 16:42:46 -0400

    Changed lower level code to uploads, now does it directly on excel files, needs more changes... also tried to fix a few bugs with how uploads were working, moved upload functionality to upload controller instead of dashboard, seperated out code to help make clear functionality

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -89,19 +89,66 @@ class UploadsController < ApplicationController
         railspath =  Rails.root.join('..', 'uploads')
         path = railspath.to_s
         filename = uploaded_io.original_filename
-	flash[:notice] = filename
-	redirect_to :action => 'index'
-  end      
-  # GET /uploads/1/process
-  #def process
-	#@upload = Upload.find(params[:id])    
-	#sql = "LOAD DATA INFILE '" + Rails.root.join('public', 'uploads', upload.file_name) + "' INTO TABLE staging_data FIELDS TERMINATED BY ',';"
-    #ActiveRecord::Base.connection.execute(sql)
-	#upload.process_date = Time.now
-	#upload.status = "Processing"
-	#upload.save
 
-	#flash[:notice] = "Processing."
-	
-  #end
+	status = upload_file filename, path, uploaded_io
+        flash[:notice] = status
+
+        if status != "Duplicate file found in uploads, file not uploaded"
+		convert_to_stagings path, uploaded_io
+	end	
+
+	redirect_to :action => 'index'	
+  end      
+
+  def upload_file(filename, path, uploaded_io)
+	status = "Duplicate file found in uploads, file not uploaded"	
+	if !File.exists?(path + "//" + filename) && File.directory?(path)
+            File.open(path + "//" + filename, 'w') do |file|
+                input = uploaded_io.read
+                input.force_encoding('UTF-8')
+                file.write(input)
+            end
+            Upload.create(:file_name => uploaded_io.original_filename, :status => 'Not Processed', :upload_date => Time.now)
+	    status = "File has been uploaded successfully, check the uploaded files to see its processing status."
+	end
+	status
+  end
+
+  def convert_to_stagings(path, uploaded_io)
+	fields_to_insert = %w{ ID_BA DT_READ AMT_KWH DAYS_USED ContractAcct. Consumption }
+	rows_to_insert = []
+	Dir[path+"/*.xlsx"].each do |file|
+                    file_path = "#{file}"
+                    file_basename = File.basename(file, ".xlsx")
+                    xlsx = Excelx.new(file_path.to_s)
+                    $i = xlsx.sheets.length - 1
+		    xlsx.default_sheet = xlsx.sheets[0]
+		    headers = Hash.new
+		    xlsx.row(1).each_with_index {|header,i|
+		    headers[header] = i
+	            }
+ 
+
+                    ((xlsx.first_row + 1)..xlsx.last_row).each do |row|
+
+		            acctnum = xlsx.row(row)[headers['ID_BA']]
+		            date = xlsx.row(row)[headers['DT_READ']]
+			    amt_kwh = xlsx.row(row)[headers['AMT_KWH']]
+			    days_used = xlsx.row(row)[headers['DAYS_USED']]
+		            
+			    date = DateTime.new(1899,12,30) + Integer(date).days  
+			    Staging.where({"acctnum"=>acctnum, "consumption"=>amt_kwh, "days_in_month"=>days_used, "read_date"=>date}).first_or_create(:locked => false)
+                    end
+		   # to be removed depending on if want to continue using multiple sheets... 
+		   # $i = xlsx.sheets.length - 1
+                   #while $i >= 0 do
+                   #     xlsx.default_sheet = xlsx.sheets[$i]
+                   #     $i -=1
+                   # end
+                    Upload.update_all( {:status => 'Processed'}, {:file_name => uploaded_io.original_filename})
+
+        end
+
+  end
+
 end
