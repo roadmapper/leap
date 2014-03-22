ebf7051e29366619e5b94f88d67f8936a0460a62
Vinay Dandekar <leap@cassiopeia.(none)>
2014-03-22 18:28:57 -0400

    Modifying Analysis controller for utility types (work in progress...)

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -14,14 +14,25 @@ class AnalysisController < ApplicationController
     end
   end
 
+  def ready_to_analyze(utility_type_id)
+    @properties = Report::UtilityReady.paginate(:page => params[:page])
+    header = [""]
+    column_names = Report::UtilityReady.column_names
+    respond_to do |format|
+      format.js
+      format.csv { send_data csv_export(header, Report::UtilityReady.all, column_names) }
+      ajax_respond format, :section_id => "ready_to_analyze"
+    end
+  end 
+
   private
   def csv_export(header, data, fields)
-	CSV.generate do |csv|
-		csv << header #["Owner Name", "Customer Unique ID", "Company Name", "Account Number"]
-		data.each do |record|
-		    csv << record.attributes.values_at(*fields)
-		end
-	end
+    CSV.generate do |csv|
+      csv << header #["Owner Name", "Customer Unique ID", "Company Name", "Account Number"]
+      data.each do |record|
+        csv << record.attributes.values_at(*fields)
+      end
+    end
   end
 
 end
