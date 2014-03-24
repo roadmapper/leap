633469cb6b186cba03ea32eb444925643fcf392e
Vinay Dandekar <leap@cassiopeia.(none)>
2014-03-24 01:56:56 -0400

    Close to making the analysis view availabile on load

--- db/migrate/20140323235304_create_report_temp_readings_good.rb
+++ db/migrate/20140323235304_create_report_temp_readings_good.rb
@@ -2,12 +2,12 @@ class CreateReportTempReadingsGood < ActiveRecord::Migration
   def up
     self.connection.execute %Q(CREATE OR REPLACE VIEW temp_readings_good AS
 	    SELECT 
-                temp_readings.owner_name,
-                temp_readings.acctnum,
-                temp_readings.read_date,
-                temp_readings.start_date,
-                temp_readings.end_date,
-                IF(temp_readings.read_date > temp_readings.start_date
+                temp_dominion_readings.owner_name,
+                temp_dominion_readings.acctnum,
+                temp_dominion_readings.read_date,
+                temp_dominion_readings.start_date,
+                temp_dominion_readings.end_date,
+                IF(temp_dominion_readings.read_date > temp_readings.start_date
                     AND temp_readings.read_date < temp_readings.end_date, 1, NULL) AS gooddata
             FROM
                 temp_readings;
