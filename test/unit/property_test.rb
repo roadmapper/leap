a693b0b7e55f958bee0a96fb4860302edc5473a1
Jay Ashe <jga9je@virginia.edu>
2014-04-08 12:53:44 -0400

    Validation for Property and Record Lookup, and Unit Tests

--- test/unit/property_test.rb
+++ test/unit/property_test.rb
@@ -1,8 +1,29 @@
 require 'test_helper'
 
 class PropertyTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+#Property creation tests
+  test "zip creation validation" do
+  	property = Property.new :owner_name=>"test1", :street_address=>"123 fake street", :city=>"CVille", :state=>"VA"
+  	assert !property.save
+  end
+
+  test "owner_name creation validation" do
+  	property = Property.new :street_address=>"123 fake street", :city=>"CVille", :state=>"VA", :zipcode=>"22180"
+  	assert !property.save
+  end
+
+  test "street_address creation validation" do
+  	property = Property.new :owner_name=>"test1", :city=>"CVille", :state=>"VA", :zipcode=>"22180"
+  	assert !property.save
+  end
+
+  test "state creation validation" do
+  	property = Property.new :owner_name=>"test1", :street_address=>"123 fake street", :city=>"CVille", :zipcode=>"22180"
+  	assert !property.save
+  end
+
+  test "city creation validation" do
+  	property = Property.new :owner_name=>"test1", :street_address=>"123 fake street", :state=>"VA", :zipcode=>"22180"
+  	assert !property.save
   end
 end
--- test/unit/record_lookup_test.rb
+++ test/unit/record_lookup_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class RecordLookupTest < ActiveSupport::TestCase
-   test "the truth" do
-     assert true
-   end
+   #Property creation tests
+  test "property_id creation validation" do
+  	rl = RecordLookup.new :utility_type_id=>"0", :company_name=>"DOMINION", :acct_num=>"123456789"
+  	assert !rl.save
+  end
+
+  test "utility_type_id creation validation" do
+  	rl = RecordLookup.new :property_id=>"123", :company_name=>"DOMINION", :acct_num=>"123456789"
+  	assert !rl.save
+  end
+
+  test "company_name creation validation" do
+  	rl = RecordLookup.new :property_id=>"123", :utility_type_id=>"0", :acct_num=>"123456789"
+  	assert !rl.save
+  end
+
+  test "acct_num creation validation" do
+  	rl = RecordLookup.new :property_id=>"123", :utility_type_id=>"0", :company_name=>"DOMINION"
+  	assert !rl.save
+  end
 end
