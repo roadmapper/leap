172728f5114725056fbc0faf86f473d1114cd5ff
Eric Tsai <ekt7be@virginia.edu>
2014-04-08 05:50:29 -0400

    added testing in

--- test/unit/utility_type_test.rb
+++ test/unit/utility_type_test.rb
@@ -1,7 +1,56 @@
 require 'test_helper'
 
 class UtilityTypeTest < ActiveSupport::TestCase
-   test "the truth" do
-     assert true
-   end
+  
+  test "should save utilitytype given id" do
+	    @util = UtilityType.new(
+	      :id => "0")
+	    assert @util.save
+  end
+
+
+  test "should save utilitytype with all fields" do
+	    @util = UtilityType.new(
+	      :id => "0",
+	      :typeName => "electricity",
+	      :units => "Kwh")
+	    assert @util.save	
+  end
+
+  test "should not save null utilitytype" do
+	    @util = UtilityType.new
+	    assert @util.save
+  end
+  
+  test "should destroy existing utilitytype" do
+	    @util = UtilityType.new(
+	      :id => "0")
+	    @util.save
+	    @util.destroy
+	     assert_raise(ActiveRecord::RecordNotFound) {
+      User.find(@util.id)
+    }
+  end
+  
+  test "should update existing utilitytype" do
+	    @util = UtilityType.new(
+	      :id => "0")
+	    @util.save
+	    @util.id = "1"
+	    assert @util.save
+  end
+
+
+  test "should update typename, units" do
+	    @util = UtilityType.new(
+	      :id => "0",
+	      :typeName => "electricity",
+	      :units => "Kwh")
+	    @util.save
+	    @util.typeName = "test"
+	    @util.units = "test2"
+	    assert_equal "test", @util.typeName
+	    assert_equal "test2", @util.units
+  end
+
 end
