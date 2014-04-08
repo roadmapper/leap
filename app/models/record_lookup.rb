5003950f94eabf2ff6312f8c9dbdcf1e66fadcf3
Jay Ashe <jga9je@virginia.edu>
2014-04-08 12:53:44 -0400

    Validation for Property and Record Lookup, and Unit Tests

--- app/models/record_lookup.rb
+++ app/models/record_lookup.rb
@@ -1,4 +1,9 @@
 class RecordLookup < ActiveRecord::Base
+  validates :property_id, presence: true
+  validates :utility_type_id, presence: true
+  validates :company_name, presence: true
+  validates :acct_num,  presence: true
+
   belongs_to :property
   belongs_to :utility_type
   attr_accessible :acct_num, :company_name, :property_id, :utility_type_id
